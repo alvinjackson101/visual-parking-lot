37101b4da848bab61cce81777743b457377dc3c2
Alvin Jackson <alvinjackson@Alvins-MBP.attlocal.net>
2019-05-16 20:19:16 -0500

    updated road markers / navagation

--- mapping.js
+++ mapping.js
@@ -1,25 +1,46 @@
 // spaces in parking lot
+let spaceIdCounter = 1;
+let spaces = [];
+let takenSpaces = [];
 
-function ParkingSpace(top, left, width, height){
+function ParkingSpace(top, left, width, height, opening){
     this.top = top;
     this.left = left;
     this.width = width;
     this.height = height;
+    this.opening = opening;
+    this.spaceId = spaceIdCounter;
 
     this.paint = () => {
         let parkingLot = document.getElementById("parkinglot");
         console.log(parkingLot)
         let parkingSpace = document.createElement('div');
-        parkingSpace.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:${this.width}px; height:${this.height}px; border:solid 1px red;`)
+
+        if(this.opening === 'left'){
+            parkingSpace.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:${this.width}px; height:${this.height}px; border:solid 1px red; border-left:0;`)
+            
+        } else if(this.opening === 'right'){
+            parkingSpace.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:${this.width}px; height:${this.height}px; border:solid 1px red; border-right:0;`)
+            
+        } else if (this.opening === 'top'){
+            parkingSpace.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:${this.width}px; height:${this.height}px; border:solid 1px red; border-top:0;`)
+            
+        } else if (this.opening === 'bottom'){
+            parkingSpace.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:${this.width}px; height:${this.height}px; border:solid 1px red; border-bottom:0;`)
+            
+        } else {
+            parkingSpace.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:${this.width}px; height:${this.height}px; border:solid 1px red; border-bottom:0;`)
+        }
         parkingLot.appendChild(parkingSpace);
     }
+    spaceIdCounter++;
+    spaces.push(this)
 }
 
 
-function Intersection(top, left, turns){
+function Intersection(top, left){
     this.top = top;
     this.left = left;
-    this.turns = turns;
 
     this.paint = () => {
         let parkingLot = document.getElementById("parkinglot");
@@ -44,6 +65,21 @@ function Road(top, left, width, height, type){
     }
 }
 
+function Marker(top, left, width, height){
+    this.top = top;
+    this.left = left;
+    this.width = width;
+    this.height = height;
+
+
+    this.paint = () => {
+        let parkingLot = document.getElementById("parkinglot");
+        let marker = document.createElement('div');
+        marker.setAttribute ("style", `position: absolute; top:${this.top}px;  left:${this.left}px; width:10px; height:10px; border-radius:50%; background-color:purple;`)
+        parkingLot.appendChild(marker);
+    }
+}
+
 // left/right rail width:125, height: 63
 
 function createParkingLot(opening, numOfSpaces){
@@ -53,23 +89,27 @@ function createParkingLot(opening, numOfSpaces){
 
     for(i = 0; i < numOfSpaces; i++){
         if(opening === 'left'){
-            new ParkingSpace(sideCounter, 15, 125, 63).paint();
+            new ParkingSpace(sideCounter, 15, 125, 63, 'right').paint();
+            new Marker(sideCounter + 26, 193, 125, 63).paint()
             sideCounter = sideCounter + 63;
 
         } else if(opening === 'right'){
-            new ParkingSpace(sideCounter, 670, 125, 63).paint();
+            new ParkingSpace(sideCounter, 670, 125, 63, 'left').paint();
+            new Marker(sideCounter + 26, 603, 125, 63).paint()
             sideCounter = sideCounter + 63;
 
         } else if (opening === 'top'){
-            new ParkingSpace(20, middleTopRailCounter, 63, 125).paint();
+            new ParkingSpace(20, middleTopRailCounter, 63, 125, 'bottom').paint();
             middleTopRailCounter = middleTopRailCounter + 63;
 
         } else if (opening === 'middle'){
-            new ParkingSpace(240, middleRailCounter, 63, 125).paint();
+            new ParkingSpace(240, middleRailCounter, 63, 125, 'top').paint();
+            new Marker(180, middleRailCounter + 25, 63, 125).paint()
             middleRailCounter = middleRailCounter + 63;
 
         } else if (opening === 'bottom'){
-            new ParkingSpace(465, middleRailCounter, 63, 125).paint();
+            new ParkingSpace(465, middleRailCounter, 63, 125, 'top').paint();
+            new Marker(405, middleRailCounter + 25, 63, 125).paint()
             middleRailCounter = middleRailCounter + 63;
 
         } else {
@@ -85,9 +125,7 @@ createParkingLot('middle', 5)
 createParkingLot('bottom', 5)
 
 
-new Intersection(410, 190, ['north', 'south', 'east']).paint()
-new Intersection(190, 190, ['north', 'south', 'east']).paint()
-new Road(10, 190, 230, 230).paint()
-new Intersection(410, 600, ['north', 'south', 'west']).paint()
-new Intersection(190, 600, ['north', 'south', 'west']).paint()
-new Road(10, 610, 230, 230).paint()
+// new Intersection(410, 190).paint()
+// new Intersection(190, 190).paint()
+// new Intersection(410, 600).paint()
+// new Intersection(190, 600).paint()
